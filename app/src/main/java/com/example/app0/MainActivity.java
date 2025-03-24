406960f22d3108284a527d314fb0f4d8b71de820
furrbeebs <phoebel0904@gmail.com>
2025-03-25 02:48:30 +0800

    update: calendar

--- app/src/main/java/com/example/app0/MainActivity.java
+++ app/src/main/java/com/example/app0/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.app0;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
 // calendar view
 import android.os.Bundle;
 import android.widget.CalendarView;
@@ -11,7 +15,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); // the xml file
+
+        // initialise button
+        Button btn = findViewById(R.id.modebutt);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Use intent to go to activity page
+                Intent intent = new Intent(MainActivity.this);
+                startActivity(intent);
+            }
+        });
+
 
         // calendar
         CalendarView calendar = findViewById(R.id.calendar);
