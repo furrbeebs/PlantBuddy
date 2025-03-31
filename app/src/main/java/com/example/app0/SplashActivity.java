a61e020e308d01269f84c450d169ffc037494e0a
Leix2 <leix2.yy@gmail.com>
2025-03-31 17:24:30 +0800

    Added a splash screen(with a plant animation) UI; files modified: 1.build.gradle.kts and 2.AndroidManifest.xml files added: 1.raw/splash_plant.json, 2.SplashActivity.java and 3.res/layout/activity_splash.xml

---
+++ app/src/main/java/com/example/app0/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.app0;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        if(getSupportActionBar()!=null){   //add on to troubleshoot
+            getSupportActionBar().hide();
+        }
+
+        final Intent i= new Intent(SplashActivity.this,MainActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(i);
+                finish();
+
+            }
+        },1800);
+    }
+}
